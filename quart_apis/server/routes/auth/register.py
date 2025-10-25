0b08d73f293c8d1e07f428404cff136568107d47
wintun-devop <wintun.hlaing@outlook.com>
2025-10-25 23:50:02 +0630

    register api validation success

--- quart_apis/server/routes/auth/register.py
+++ quart_apis/server/routes/auth/register.py
@@ -1,16 +1,18 @@
 import uuid
 from quart import jsonify,make_response,Blueprint,request
 from pydantic import ValidationError
-
-
+from quart_schema import validate_request, validate_response
+from sqlalchemy.exc import IntegrityError
+import asyncpg
 
 # from server.utils.hash import hash_password
 from server.utils.argon2_hash import hash_password_argon2
 from server.models.db import get_write_session
 from server.utils.unique_string import unique_string
 from server.resources.api_paths import USER_REGISER
-from server.schema.users_schema import UserCreate,UserRead,UserUpdate,UserCustomRead
+from server.schema.users_schema import UserCreate,UserCustomRead
 from server.services.users_services import user_create
+
 
 #jwt function import
 from quart_jwt_extended import (
@@ -28,29 +30,28 @@ from quart_jwt_extended import (
 #declare blue print
 register_bp = Blueprint('register',__name__,url_prefix=USER_REGISER)
 @register_bp.route("/",methods=['POST'])
-async def create_user():
+@validate_request(UserCreate)
+@validate_response(UserCustomRead,201)
+async def create_user(data:UserCreate):
     try:
-        req_body = await request.get_json()
-        validate_body = UserCreate.model_validate(req_body)
-        validate_values = validate_body.model_dump()
-        # hash_pass =await hash_password(validate_values["password"])
-        hash_pass =await hash_password_argon2(validate_values["password"])
-        data = {
+        hash_pass =await hash_password_argon2(data.password)
+        payload = {
             "id":str(uuid.uuid4()),
-            "email":validate_values["email"],
+            "email":data.email,
             "password":hash_pass,
             "username":unique_string("usr")
         }
         async for session in get_write_session():
-            user = await user_create(session, data)
-            return await make_response(jsonify(UserCustomRead.model_validate(user).model_dump(mode="json")), 201)
-    except ValidationError as e:
-        return await make_response(jsonify({"error": e.errors()}), 400)
+            user = await user_create(session, payload)
+            resp = UserCustomRead.model_validate(user).model_dump(mode="json")
+            return resp, 201
+    except IntegrityError as err:
+            print("un",err)
+            return {"status": "fail","msg": "unique constraint"}, 400
     except Exception as e:
         print("eee",e)
         # db_session.rollback()
         error = {"status": "fail", "msg": "An unexpected error occurred"}
-        return await make_response(jsonify(error), 500)
-
+        return error, 500
 
 
